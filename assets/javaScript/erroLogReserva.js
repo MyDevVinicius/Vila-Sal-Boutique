8ff1ee8c3dfda2b424274f91ec6b7588efb64dd8
Vinicius Nascimento <nascimentovrn@outlook.com>
2024-06-09 17:27:37 -0400

    teste de integração

--- assets/javaScript/erroLogReserva.js
+++ assets/javaScript/erroLogReserva.js
@@ -1,26 +1,24 @@
-const url = 'https://secured.sirvoy.com/widget/book_widget_cors.js';
+ const url = 'https://secured.sirvoy.com/widget/book_widget_cors.js';
 
-fetch(url)
-  .then(response => {
-    if (response.ok) {
-      return response.text();
-    } else {
-      throw new Error('Erro de rede ao carregar o widget Sirvoy');
-    }
-  })
-  .then(data => {
-    if (data.includes('SirvoyBookWidget')) {
-      exibirMensagem('Integração com reservas bem-sucedida!');
-    } else {
-      throw new Error('O widget Sirvoy não foi carregado corretamente');
-    }
-  })
-  .catch(error => {
-    exibirMensagem('Erro ao integrar com reservas: ' + error.message);
-  });
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.text();
+      } else {
+        throw new Error('Erro de rede ao carregar o widget Sirvoy');
+      }
+    })
+    .then(data => {
+      if (data.includes('SirvoyBookWidget')) {
+        exibirMensagem('Integração com reservas bem-sucedida!');
+      } else {
+        throw new Error('O widget Sirvoy não foi carregado corretamente');
+      }
+    })
+    .catch(error => {
+      exibirMensagem('Erro ao integrar com reservas: ' + error.message);
+    });
 
-function exibirMensagem(mensagem) {
-  const mensagemElement = document.createElement('div');
-  mensagemElement.textContent = mensagem;
-  document.body.appendChild(mensagemElement);
-}
+  function exibirMensagem(mensagem) {
+   alert(exibirMensagem(mensagem));
+  }
